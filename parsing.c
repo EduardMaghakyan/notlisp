07e7807ae965b5566da5c2c9c78ef858a17f688e
Eduard Maghkayan <edi.maghakyan@gmail.com>
2018-06-19 23:27:42 +0200

    Read and evaluate simple math functions

--- parsing.c
+++ parsing.c
@@ -20,6 +20,39 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  if (strcmp(op, "%") == 0) { return x % y; }
+  if (strcmp(op, "^") == 0) { return x ^ y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+  /* If tagged as number return it*/
+  if (strstr(t->tag, "number")) {
+	return atoi(t->contents);
+  }
+
+  /* Select operator */
+  char* op = t->children[1]->contents;
+  
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Combine the rest*/
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  
+  return x;
+}
+
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
@@ -28,7 +61,7 @@ int main(int argc, char** argv) {
   /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
     " number       : /-?[0-9]+\\.?[0-9]*/ ;"
-    " operator     : /[\\+\\-\\*\\/\\%]|add|sub|mul|div/ ;"
+    " operator     : /[\\+\\-\\*\\/\\%\\^]|add|sub|mul|div/ ;"
     " expr         : <number> | '(' <operator> <expr>+ ')' ;"
     " notlispy    : /^/ <operator> <expr>+ /$/ ;   ",
   Number, Operator, Expr, NotLispy);
@@ -39,7 +72,9 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, NotLispy, &r)) {
       /* On success print and delete the AST */
-      mpc_ast_print(r.output);
+      // mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print and delete the Error */
