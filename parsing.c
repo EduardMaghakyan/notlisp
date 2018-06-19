96d4ea6992d5f617cb6471cf33bd8a0b3b63b253
Eduard Maghakyan <eduard.maghakyan@momox.biz>
2018-06-19 14:01:30 +0200

    Added polish notation parser

--- parsing.c
+++ parsing.c
@@ -1,23 +1,57 @@
 #include "mpc.h"
 
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
 int main(int argc, char** argv) {
-  mpc_parser_t* Adjective = mpc_new("adjective");
-  mpc_parser_t* Noun = mpc_new("noun");
-  mpc_parser_t* Phrase = mpc_new("phrase");
-  mpc_parser_t* Doge = mpc_new("doge");
-
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* NotLispy = mpc_new("notlispy");
+  /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
-  "                                           \
-      adjective : \"wow\" | \"many\"            \
-                |  \"so\" | \"such\";           \
-      noun      : \"lisp\" | \"language\"       \
-                | \"book\" | \"build\" | \"c\"; \
-      phrase    : <adjective> <noun>;           \
-      doge      : <phrase>*;                    \
-    ",
-    Adjective, Noun, Phrase, Doge
-  );
+    " number       : /-?[0-9]+\\.?[0-9]*/ ;"
+    " operator     : /[\\+\\-\\*\\/\\%]|add|sub|mul|div/ ;"
+    " expr         : <number> | '(' <operator> <expr>+ ')' ;"
+    " notlispy    : /^/ <operator> <expr>+ /$/ ;   ",
+  Number, Operator, Expr, NotLispy);
 
-  mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+  while(1) {
+    char* input = readline("not-lisp > ");
+    add_history(input);
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, NotLispy, &r)) {
+      /* On success print and delete the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise print and delete the Error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    free(input);
+  }
+
+
+  mpc_cleanup(4, Number, Operator, Expr, NotLispy);
   return 0;
 }
+
