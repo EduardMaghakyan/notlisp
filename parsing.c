c668112dff1f21bc958b1dcd1c50f525b37043fc
Eduard Maghakyan <eduard.maghakyan@momox.biz>
2018-07-23 18:21:43 +0200

    one step ahead

--- parsing.c
+++ parsing.c
@@ -104,16 +104,16 @@ lval* lval_add(lval* v, lval* x) {
 }
 
 lval* lval_read(mpc_ast_t* t) {
-
+  
   /* If Symbol or Number return conversion to that type */
   if (strstr(t->tag, "number")) { return lval_read_num(t); }
   if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
-
+  
   /* If root (>) or sexpr then create empty list */
   lval* x = NULL;
-  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); } 
   if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
-
+  
   /* Fill this list with any valid expression contained within */
   for (int i = 0; i < t->children_num; i++) {
     if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
@@ -121,7 +121,7 @@ lval* lval_read(mpc_ast_t* t) {
     if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
     x = lval_add(x, lval_read(t->children[i]));
   }
-
+  
   return x;
 }
 
@@ -190,6 +190,11 @@ lval* builtin_op(lval* a, char* op) {
   // Get frist element;
   lval* x = lval_pop(a, 0);
 
+  /* If no arguments and sub then perform unary negation */
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
+  }
+
   while(a->count > 0) {
     // pop next element
     lval* y = lval_pop(a, 0);
@@ -214,38 +219,38 @@ lval* builtin_op(lval* a, char* op) {
 }
 
 lval* lval_eval_sexpr(lval* v) {
-  /* Evaluate children */
-  for (int i=0; i < v->count; i++) {
+  /* Evaluate Children */
+  for (int i = 0; i < v->count; i++) {
     v->cell[i] = lval_eval(v->cell[i]);
   }
-
+  
   /* Error Checking */
-  for(int i=0; i< v->count; i++) {
-    if (v->cell[i]->type == LVAL_ERR) {
-      return lval_take(v, i);
-    }
+  for (int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) { 
+      return lval_take(v, i); 
+      }
   }
   
-  /* Empty expression */
+  /* Empty Expression */
   if (v->count == 0) {
     return v;
   }
-
-  /* Single expresssion */
-  if (v->count == 1) {
+  
+  /* Single Expression */
+  if (v->count == 1) { 
     return lval_take(v, 0);
   }
-
-  lval *f = lval_pop(v, 0);
-  if (f->type == LVAL_SYM) {
-    lval_del(f);
-    lval_del(v);
-    return lval_err("S-Expression doesn't start with symbol!");
+  
+  /* Ensure First Element is Symbol */
+  lval* f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression Does not start with symbol.");
   }
-
+  
+  /* Call builtin with operator */
   lval* result = builtin_op(v, f->sym);
   lval_del(f);
-
   return result;
 }
 
