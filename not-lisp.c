4e2402258dec2994c65d333c3f8f544360483896
Eduard Maghakyan <eduard.maghakyan@momox.biz>
2018-09-04 13:44:42 +0200

    Fixed not empty assert

--- not-lisp.c
+++ not-lisp.c
@@ -34,10 +34,9 @@ void add_history(char *unused) {}
           "Function '%s' passed incorrect type for argument %i. Got %s, Expected %s.", \
           func, index, ltype_name(args->cell[index]->type), ltype_name(expect))
 
-#define LASSERT_NOT_EMPTY(func, args, num)                                            \
-  LASSERT(args, args->count == num,                                                   \
-          "Function '%s' passed incorrect number of arguments. Got %i, Expected %i.", \
-          func, args->count, num)
+#define LASSERT_NOT_EMPTY(func, args, index)   \
+  LASSERT(args, args->cell[index]->count != 0, \
+          "Function '%s' passed {} for argument %i.", func, index);
 
 #define LASSERT_COUNT(func, args, num)                                                \
   LASSERT(args, args->count == num,                                                   \
